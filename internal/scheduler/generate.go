4feb6fe45741ff4f4939ed57bc97cbf0dd6b3db1
kevin <kevinfeng77@yahoo.com>
2025-07-19 02:58:42 -0400

    setup

---
+++ internal/scheduler/generate.go
@@ -0,0 +1,6 @@
+// generate.go
+
+package scheduler
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=models.cfg.yml scheduler.openapi.yml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=server.cfg.yml scheduler.openapi.yml
